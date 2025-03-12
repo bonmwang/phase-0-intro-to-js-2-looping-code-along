c9987a7bd1f45ea49266ec6bab128377eca9f8ee
Boniface <bm715452@gmail.com>
2025-03-13 02:15:35 +0300

    Loopig Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Alice", "Bob", "Charlie"];
+const event = "birthady";
+const messages = writeCards(names, event);
+console.log(messages);
+
+// writeCards function
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  // countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
